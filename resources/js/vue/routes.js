77ce27138976c885da5d9709407c4b0818311057
easybigi <gal.irt@easy.bi>
2023-05-21 02:59:27 +0200

    Dockerise

--- resources/js/vue/routes.js
+++ resources/js/vue/routes.js
@@ -7,23 +7,24 @@ import Login from './views/Login'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  base: process.env.BASE_URL,
-  routes: [
-    { path: '/admin', name: 'admin', component: Admin },
-    { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'client', component: Client},
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{path: '/admin', name: 'admin', component: Admin}, {
+        path: '/login',
+        name: 'login',
+        component: Login
+    }, {path: '/', name: 'client', component: Client},]
 })
 
 // global route guard
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = localStorage.getItem('token')
-  if (to.name !== 'login' && !isAuthenticated) {
-    // redirect to login page if not authenticated
-    next({ name: 'login' })
-  } else {
-    next()
-  }
+    const isAuthenticated = localStorage.getItem('token')
+    if (to.name !== 'login' && !isAuthenticated) {
+        // redirect to login page if not authenticated
+        next({name: 'login'})
+    } else {
+        next()
+    }
 })
 
 export default router
